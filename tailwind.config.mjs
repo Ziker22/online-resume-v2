c2d5373ae08d4a33f578bc6db344fbc08eeeb9de
Juraj Slavicek <juraj.slavicek@the7bridges.com>
2024-05-13 16:21:18 +0200

    Intro component & cleanup

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,12 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+import defaultTheme from "tailwindcss/defaultTheme";
+
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     colors: {
       primary: "#c3f73a",
       secondary: "#95e06c",
-      background: "#1c1018",
-      white: "#ffffff",
+      background: "#000",
+      text: "#fff",
+      "text-secondary": "#ced4da",
+    },
+    fontFamily: {
+      sans: ["Inter Tight Variable", ...defaultTheme.fontFamily.sans],
     },
     extend: {},
   },

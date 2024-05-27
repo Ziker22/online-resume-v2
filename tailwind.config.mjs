e4afdb230082a6083ca9154619ce2f51eb35a5e5
Juraj Slavicek <juraj.slavicek@gmail.com>
2024-05-27 23:00:22 +0200

    Testimonials & contact skeleton

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,8 +5,9 @@ export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     colors: {
-      primary: '#c3f73a',
-      secondary: '#094d92',
+      primary: '#f72585',
+      secondary: '#7209b7',
+      tertiary: '#3a0ca3',
       background: '#000',
       backgroundSecondary: '#111',
       text: '#fff',

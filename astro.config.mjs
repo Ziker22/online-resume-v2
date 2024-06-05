54d507896a790f67c47b2bae279ec6a2b362f0fc
Juraj Slavicek <juraj.slavicek@gmail.com>
2024-06-05 15:04:17 +0200

    One more try

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
 import { defineConfig } from 'astro/config';
 import { astroImageTools } from 'astro-imagetools';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/serverless';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), astroImageTools],
-  output: 'server',
-  adapter: vercel(),
 });

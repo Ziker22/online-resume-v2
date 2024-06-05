67920d3911c3a0a3d945ec21a7cdba993390d99b
Juraj Slavicek <juraj.slavicek@gmail.com>
2024-06-05 14:36:58 +0200

    Add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,15 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 import { imageService } from '@unpic/astro/service';
+import vercel from '@astrojs/vercel/serverless';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  output: 'server',
+  adapter: vercel(),
   image: {
     service: imageService({
+      fallbackService: 'vercel',
       placeholder: 'blurhash',
     }),
   },

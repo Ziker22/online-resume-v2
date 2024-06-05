6efa04fb67e890bfab1e4ab6b7c10807785daa39
Juraj Slavicek <juraj.slavicek@gmail.com>
2024-06-05 15:00:38 +0200

    Re add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 import { astroImageTools } from 'astro-imagetools';
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/serverless';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), astroImageTools],
+  output: 'server',
+  adapter: vercel(),
 });

3f10727694757627b1e64506af6d4442b71bd1e0
Juraj Slavicek <juraj.slavicek@gmail.com>
2024-06-06 16:31:56 +0200

    Touch up

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,19 +4,20 @@ import defaultTheme from 'tailwindcss/defaultTheme';
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    colors: {
-      primary: '#f72585',
-      secondary: '#7209b7',
-      tertiary: '#3a0ca3',
-      background: '#000',
-      backgroundSecondary: '#111',
-      text: '#fff',
-      'text-secondary': '#ced4da',
+    extend: {
+      colors: {
+        primary: '#f72585',
+        secondary: '#7209b7',
+        tertiary: '#3a0ca3',
+        background: '#000',
+        backgroundSecondary: '#111',
+        text: '#fff',
+        'text-secondary': '#ced4da',
+      },
     },
     fontFamily: {
       sans: ['Inter Tight Variable', ...defaultTheme.fontFamily.sans],
     },
-    extend: {},
   },
   plugins: [],
 };

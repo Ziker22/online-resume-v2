247ef6815eab5707151092e58159114597d712ff
Juraj Slavicek <juraj.slavicek@gmail.com>
2024-06-12 16:45:42 +0200

    Make argos screenshots more stable (#11)
    * Rename index page e2e spec
    
    * Stabilize page

--- cypress/e2e/indexPage.cy.ts
+++ cypress/e2e/indexPage.cy.ts
@@ -1,6 +1,7 @@
-describe('template spec', () => {
+describe('Take index page screenshots', () => {
   it('passes', () => {
     cy.visit('http://localhost:4321/');
+    cy.stopAndResetAllVideosToBeginning();
     cy.argosScreenshot('homepage', {
       viewports: ['ipad-2', 'iphone-se2', 'macbook-13'],
     });

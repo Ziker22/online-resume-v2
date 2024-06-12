fc9bc8312b958a04b178e506a7a42994ff18f0e4
Juraj Slavicek <juraj.slavicek@gmail.com>
2024-06-12 16:40:29 +0200

    Stabilize page

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,1 +1,21 @@
 /// <reference types="cypress" />
+
+declare namespace Cypress {
+  interface Chainable {
+    stopAndResetAllVideosToBeginning(): void;
+  }
+}
+
+Cypress.Commands.add('stopAndResetAllVideosToBeginning', () => {
+  cy.window().then((window) => {
+    const maybeVideoTags = window.document.getElementsByTagName('video');
+    if (maybeVideoTags.length > 0) {
+      cy.get('video').then((video) => {
+        for (let i = 0; i < video.length; i++) {
+          video[i].pause();
+          video[i].currentTime = 0;
+        }
+      });
+    }
+  });
+});
